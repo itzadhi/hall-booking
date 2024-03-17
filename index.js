1d91bfae9c965b3733ce9040f2cdcf04b0dd8321
adhit7 <adhithyanalan@gmail.com>
2024-03-17 14:27:44 +0530

    minor changes

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hall Booking API is running....');
+});
+
 app.use('/room', hallBookingRoutes);
 
 const PORT = process.env.PORT || 4000;
